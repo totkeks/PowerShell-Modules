bc57ea3ff86b79f1b5aff9b7779e31532a500336
Norman Dankert <norman.dankert@outlook.com>
2024-04-13 01:37:39 +0200

    Improve performance for Find-Repository

--- src/totkeks.GitManagement/Repository/Find-Repository.ps1
+++ src/totkeks.GitManagement/Repository/Find-Repository.ps1
@@ -15,32 +15,33 @@ function Find-Repository {
 		Write-Debug "Updating repository cache"
 
 		$baseDirectory = Get-BaseDirectory
-		$repositoryPaths = ($baseDirectory | FindDotGitDirectory).FullName
-		$providers = Get-Provider
+
+		$GitManagement.Repositories = Get-Provider | ForEach-Object {
+			$provider = $_
+
+			(Join-Path $baseDirectory $_.Name) | FindDotGitDirectory | ForEach-Object {
+				$splitPath = [IO.Path]::GetRelativePath($baseDirectory, $_).Split([IO.Path]::DirectorySeparatorChar)
+				$directories = $splitPath[1..($splitPath.Count - 1)]
+
+				$repository = [PSCustomObject]@{
+					PSTypeName = "GitManagement.GitRepository"
+					Provider = $provider.Name
+					Path = $_
+				}
+
+				$properties = [ordered]@{ }
+				for ($ii = 0; $ii -lt $directories.Length; $ii++) {
+					$hierarchicalDirectory = $provider.DirectoryHierarchy[$ii]
+					$properties.Add($hierarchicalDirectory, $directories[$ii])
+				}
+
+				$repository | Add-Member Properties $properties
+				$repository
+			}
+		}
 
 		$GitManagement.LastRepositoryScan = Get-Date
-		$GitManagement.Repositories = $repositoryPaths | ForEach-Object {
-			$splitPath = [io.path]::GetRelativePath($baseDirectory, $_).Split([io.path]::DirectorySeparatorChar)
-			$provider = $providers | Where-Object Name -EQ $splitPath[0]
-			$directories = $splitPath[1..($splitPath.Count - 1)]
-
-			$repository = [PSCustomObject]@{
-				PSTypeName = "GitManagement.GitRepository"
-				Provider = $provider.Name
-				Path = $_
-			}
-
-			$properties = [ordered]@{ }
-			for ($ii = 0; $ii -lt $directories.Length; $ii++) {
-				$hierarchicalDirectory = $provider.DirectoryHierarchy[$ii]
-				$properties.Add($hierarchicalDirectory, $directories[$ii])
-			}
-
-			$repository | Add-Member Properties $properties
-			$repository
-		}
 	}
 
 	$GitManagement.Repositories
 }
-
