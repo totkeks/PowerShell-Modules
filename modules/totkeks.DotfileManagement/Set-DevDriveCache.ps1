3c3728588dfebc1f06c846676aaa1d1505841668
Norman Dankert <norman.dankert@outlook.com>
2025-02-14 18:20:56 +0100

    feat: add more cache directories to dev drive

--- modules/totkeks.DotfileManagement/Set-DevDriveCache.ps1
+++ modules/totkeks.DotfileManagement/Set-DevDriveCache.ps1
@@ -1,43 +1,71 @@
 function Set-DevDriveCache {
 	[CmdletBinding()]
 	param (
-		[Parameter(Mandatory, HelpMessage = "Path to the dev drive.")]
+		[Parameter(Mandatory, HelpMessage = 'Path to the dev drive.')]
 		[ValidateScript({ Test-Path $_ })]
 		[string]$Path
 	)
 
-	Write-Verbose "Configuring dev drive caches"
+	Write-Verbose 'Configuring dev drive caches'
 
-	$basePath = Join-Path $Path "packages"
+	$basePath = Join-Path $Path 'packages'
 	New-Item -Path $basePath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
-	$npmPath = Join-Path $basePath "npm"
+	#region JavaScript & TypeScript
+	$npmPath = Join-Path $basePath 'npm'
 	Write-Verbose "Setting npm cache to $npmPath"
 	New-Item -Path $npmPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	[Environment]::SetEnvironmentVariable("npm_config_cache", $npmPath, [EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('npm_config_cache', $npmPath, [EnvironmentVariableTarget]::User)
 
-	$nugetPath = Join-Path $basePath "nuget"
+	$yarnPath = Join-Path $basePath 'yarn'
+	Write-Verbose "Setting Yarn cache to $yarnPath"
+	New-Item -Path $yarnPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+	[Environment]::SetEnvironmentVariable('YARN_CACHE_FOLDER', $yarnPath, [EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('YARN_ENABLE_GLOBAL_CACHE', 'true', [EnvironmentVariableTarget]::User)
+	#endregion
+
+	#region .NET
+	$nugetPath = Join-Path $basePath 'nuget'
 	Write-Verbose "Setting NuGet cache to $nugetPath"
 	New-Item -Path $nugetPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	[Environment]::SetEnvironmentVariable("NUGET_PACKAGES", $nugetPath, [EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('NUGET_PACKAGES', $nugetPath, [EnvironmentVariableTarget]::User)
+	#endregion
 
-	$vcpkgPath = Join-Path $basePath "vcpkg"
+	#region C & C++
+	$vcpkgPath = Join-Path $basePath 'vcpkg'
 	Write-Verbose "Setting Vcpkg cache to $vcpkgPath"
 	New-Item -Path $vcpkgPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	[Environment]::SetEnvironmentVariable("VCPKG_DEFAULT_BINARY_CACHE", $vcpkgPath, [EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('VCPKG_DEFAULT_BINARY_CACHE', $vcpkgPath, [EnvironmentVariableTarget]::User)
+	#endregion
 
-	$pipPath = Join-Path $basePath "pip"
+	#region Python
+	$pipPath = Join-Path $basePath 'pip'
 	Write-Verbose "Setting pip cache to $pipPath"
 	New-Item -Path $pipPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	[Environment]::SetEnvironmentVariable("PIP_CACHE_DIR", $pipPath, [EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('PIP_CACHE_DIR', $pipPath, [EnvironmentVariableTarget]::User)
 
-	$cargoPath = Join-Path $basePath "cargo"
+	$poetryPath = Join-Path $basePath 'poetry'
+	Write-Verbose "Setting Poetry cache to $poetryPath"
+	New-Item -Path $poetryPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+	[Environment]::SetEnvironmentVariable('POETRY_CACHE_DIR', $poetryPath, [EnvironmentVariableTarget]::User)
+
+	$uvPath = Join-Path $basePath 'uv'
+	Write-Verbose "Setting uv cache to $uvPath"
+	New-Item -Path $uvPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+	[Environment]::SetEnvironmentVariable('UV_CACHE_DIR', $uvPath, [EnvironmentVariableTarget]::User)
+	#endregion
+
+	#region Rust
+	$cargoPath = Join-Path $basePath 'cargo'
 	Write-Verbose "Setting Cargo cache to $cargoPath"
 	New-Item -Path $cargoPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	[Environment]::SetEnvironmentVariable("CARGO_HOME", $cargoPath, [EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('CARGO_HOME', $cargoPath, [EnvironmentVariableTarget]::User)
+	#endregion
 
-	$mavenPath = Join-Path $basePath "maven"
+	#region Java
+	$mavenPath = Join-Path $basePath 'maven'
 	Write-Verbose "Setting Maven cache to $mavenPath"
 	New-Item -Path $mavenPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	[Environment]::SetEnvironmentVariable("MAVEN_OPTS", "-Dmaven.repo.local=$mavenPath", [EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('MAVEN_OPTS', "-Dmaven.repo.local=$mavenPath", [EnvironmentVariableTarget]::User)
+	#endregion
 }
